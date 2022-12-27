831a9efdee805f0009224d3a1be167c6e9a8f30e
Vladimir RM <shvdrummer@gmail.com>
2022-12-27 14:57:20 +0200

    add images

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
 const canvas = document.querySelector("#game");
 
 const stx = canvas.getContext("2d");
+
+const ground = new Image();
+
+ground.src = "/img/field.png";
+
+const food = new Image();
+
+ground.src = "/img/food.png";
